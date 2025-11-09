70c8719e0240a8c7b20c7107c116b93cb805ec28
Stefon Alfaro <stefonalfaro@gmail.com>
2025-11-09 13:01:07 -0600

    Education fade removed

--- src/components/educationCard/EducationCard.js
+++ src/components/educationCard/EducationCard.js
@@ -1,10 +1,20 @@
-import React, {createRef, useContext} from "react";
+import React, {createRef, useContext, useState, useEffect} from "react";
 import {Fade, Slide} from "react-reveal";
 import "./EducationCard.scss";
 import StyleContext from "../../contexts/StyleContext";
 
 export default function EducationCard({school}) {
   const imgRef = createRef();
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkMobile = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+    checkMobile();
+    window.addEventListener('resize', checkMobile);
+    return () => window.removeEventListener('resize', checkMobile);
+  }, []);
 
   const GetDescBullets = ({descBullets}) => {
     return descBullets
@@ -19,54 +29,65 @@ export default function EducationCard({school}) {
 
   if (!school.logo)
     console.error(`Image of ${school.name} is missing in education section`);
-  return (
-    <div>
-      <Fade left duration={1000}>
-        <div className="education-card">
-          {school.logo && (
-            <div className="education-card-left">
-              <img
-                crossOrigin={"anonymous"}
-                ref={imgRef}
-                className="education-roundedimg"
-                src={school.logo}
-                alt=""
-              />
-            </div>
-          )}
-          <div className="education-card-right">
-            <h5 className="education-text-school">{school.schoolName}</h5>
 
-            <div className="education-text-details">
-              <h5
-                className={
-                  isDark
-                    ? "dark-mode education-text-subHeader"
-                    : "education-text-subHeader"
-                }
-              >
-                {school.subHeader}
-              </h5>
-              <p
-                className={`${
-                  isDark ? "dark-mode" : ""
-                } education-text-duration`}
-              >
-                {school.duration}
-              </p>
-              <p className="education-text-desc">{school.desc}</p>
-              <div className="education-text-bullets">
-                <ul>
-                  <GetDescBullets descBullets={school.descBullets} />
-                </ul>
-              </div>
-            </div>
+  const cardContent = (
+    <div className="education-card">
+      {school.logo && (
+        <div className="education-card-left">
+          <img
+            crossOrigin={"anonymous"}
+            ref={imgRef}
+            className="education-roundedimg"
+            src={school.logo}
+            alt=""
+          />
+        </div>
+      )}
+      <div className="education-card-right">
+        <h5 className="education-text-school">{school.schoolName}</h5>
+
+        <div className="education-text-details">
+          <h5
+            className={
+              isDark
+                ? "dark-mode education-text-subHeader"
+                : "education-text-subHeader"
+            }
+          >
+            {school.subHeader}
+          </h5>
+          <p
+            className={`${
+              isDark ? "dark-mode" : ""
+            } education-text-duration`}
+          >
+            {school.duration}
+          </p>
+          <p className="education-text-desc">{school.desc}</p>
+          <div className="education-text-bullets">
+            <ul>
+              <GetDescBullets descBullets={school.descBullets} />
+            </ul>
           </div>
         </div>
-      </Fade>
-      <Slide left duration={2000}>
-        <div className="education-card-border"></div>
-      </Slide>
+      </div>
+    </div>
+  );
+
+  const borderContent = <div className="education-card-border"></div>;
+
+  return (
+    <div>
+      {isMobile ? cardContent : (
+        <Fade left duration={1000}>
+          {cardContent}
+        </Fade>
+      )}
+      {isMobile ? borderContent : (
+        <Slide left duration={2000}>
+          {borderContent}
+        </Slide>
+      )}
     </div>
   );
 }
